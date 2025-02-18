9c2733cb2082a577bb041fba6b745bc67d1368d0
Brian Travis <bztravis88@gmail.com>
2025-02-18 01:21:47 -0500

    Misc improvements

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -17,54 +17,58 @@ export default function Page() {
       <title>{getPageTitle('Login')}</title>
 
       <div className={styles.LoginRoot}>
-        <Logo size="md" />
+        <Link href="/">
+          <Logo size="md" />
+        </Link>
 
         <div className={styles.FormContainer}>
           <div className={styles.widthClamp}>
             {/* <HatWidthClamp sizeRem={pxToRem(280)}> */}
-            <HatFlex.Col align="stretch" gap="lg">
+            <HatFlex.Col gap="xl">
               <HatText.h1 size="xl">Welcome back!</HatText.h1>
 
-              <HatButton
-                size="lg"
-                color="secondary"
-                onClick={() => console.log('clicked')}
-              >
-                <Google />
-                Continue with Google
-              </HatButton>
+              <HatFlex.Col align="stretch" gap="lg">
+                <HatButton
+                  size="lg"
+                  color="secondary"
+                  onClick={() => console.log('clicked')}
+                >
+                  <Google />
+                  Continue with Google
+                </HatButton>
 
-              <HatBreak paddingVertical="none" />
+                <HatBreak paddingVertical="none" />
 
-              <HatFlex.Col gap="md">
-                <HatTextInput
-                  label="Email"
-                  placeholder="Email"
-                  type="email"
-                  // required
-                />
+                <HatFlex.Col gap="md">
+                  <HatTextInput
+                    label="Email"
+                    placeholder="Email"
+                    type="email"
+                    // required
+                  />
 
-                <HatTextInput
-                  label="Password"
-                  placeholder="Password"
-                  type="password"
-                  // required
-                />
-              </HatFlex.Col>
+                  <HatTextInput
+                    label="Password"
+                    placeholder="Password"
+                    type="password"
+                    // required
+                  />
+                </HatFlex.Col>
 
-              <HatButton size="lg" onClick={() => console.log('clicked')}>
-                Login
-              </HatButton>
+                <HatButton size="lg" onClick={() => console.log('clicked')}>
+                  Login
+                </HatButton>
 
-              <HatFlex.Col gap="xs">
-                <HatText.p>
-                  Forgot your password?{' '}
-                  <Link href="/login">Reset password</Link>
-                </HatText.p>
+                <HatFlex.Col gap="xs">
+                  <HatText.p>
+                    Forgot your password?{' '}
+                    <Link href="/login">Reset password</Link>
+                  </HatText.p>
 
-                <HatText.p>
-                  Don’t have an account? <Link href="/login">Sign up</Link>
-                </HatText.p>
+                  <HatText.p>
+                    Don’t have an account? <Link href="/login">Sign up</Link>
+                  </HatText.p>
+                </HatFlex.Col>
               </HatFlex.Col>
             </HatFlex.Col>
             {/* </HatWidthClamp> */}
