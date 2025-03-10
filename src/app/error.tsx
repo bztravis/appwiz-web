0ee813f059518491ee42f95848822e5bd8170faa
Brian Travis <bztravis@umich.edu>
2025-03-10 02:09:45 -0400

    Add better not found and error pages

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -5,8 +5,12 @@ import { HatFlex } from '@/Hat/HatFlex'
 import SkullLine from '../assets/icons/SkullLine.svg'
 import styles from './error.module.scss'
 import { HatText } from '@/Hat/HatText'
+import { HatButton } from '@/Hat/HatButton'
+import { useRouter } from 'next/navigation'
 
 export default function Error() {
+  const router = useRouter()
+
   return (
     <FullPageLayout>
       <HatFlex.Col gap="md" align="center">
@@ -17,6 +21,8 @@ export default function Error() {
         <HatText.p size="md" align="center">
           Oops, an error occurred. Try again later.
         </HatText.p>
+
+        <HatButton onClick={() => router.back()}>Go back</HatButton>
       </HatFlex.Col>
     </FullPageLayout>
   )
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,29 @@
 'use client'
 
+import FullPageLayout from '@/components/FullPageLayout'
+import { HatFlex } from '@/Hat/HatFlex'
+import QuestionLine from '../assets/icons/QuestionLine.svg'
+import { HatText } from '@/Hat/HatText'
+import styles from './not-found.module.scss'
+import { HatButton } from '@/Hat/HatButton'
+import { useRouter } from 'next/navigation'
+
 export default function Error() {
-  return <div>404</div>
+  const router = useRouter()
+
+  return (
+    <FullPageLayout>
+      <HatFlex.Col gap="md" align="center">
+        <span className={styles.icon}>
+          <QuestionLine />
+        </span>
+
+        <HatText.p size="md" align="center">
+          Hmm... we couldn’t find the page you’re looking for.
+        </HatText.p>
+
+        <HatButton onClick={() => router.back()}>Go back</HatButton>
+      </HatFlex.Col>
+    </FullPageLayout>
+  )
 }
