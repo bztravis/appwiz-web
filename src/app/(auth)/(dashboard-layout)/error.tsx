c0c43596815ef2769eac9c0c61c074c86db7fb9d
Brian Travis <bztravis@umich.edu>
2025-03-16 15:46:44 -0400

    Add dashboard error boundary and not found boundary

---
+++ src/app/(auth)/(dashboard-layout)/error.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { EmptyState } from '@/components/EmptyState'
+import { useRouter } from 'next/navigation'
+import SkullLine from '../../../assets/icons/SkullLine.svg'
+import { HatFullHeight } from '@/Hat/HatFullHeight'
+
+export default function Error() {
+  const router = useRouter()
+
+  return (
+    <HatFullHeight>
+      <EmptyState
+        icon={<SkullLine />}
+        message="Oops, an error occurred. Try again later."
+        actions={[{ label: 'Go back', onClick: () => router.back() }]}
+      />
+    </HatFullHeight>
+  )
+}
---
+++ src/app/(auth)/(dashboard-layout)/organizations/[slug]/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { notFound } from 'next/navigation'
+
+export default function Page() {
+  throw new Error('uh oh')
+
+  return <div>hello</div>
+}
