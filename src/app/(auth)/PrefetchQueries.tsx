74400235fc0fdfb275fd14326f9759cd8aa1a25d
Brian Travis <bztravis@umich.edu>
2025-03-17 21:09:01 -0400

    Fix dashboard layout height 100dvh

--- src/app/(auth)/PrefetchQueries.tsx
+++ src/app/(auth)/PrefetchQueries.tsx
@@ -2,14 +2,24 @@
 
 import { useSuspenseQuery } from '@tanstack/react-query'
 
+let done = false
+
 export function PrefectchQueries() {
-  const { data, isError } = useSuspenseQuery({
-    queryKey: ['2'],
-    queryFn: async () => {
-      await new Promise((resolve) => setTimeout(resolve, 3000))
-      return { name: 'John Doe' }
-    },
-  })
+  if (!done)
+    throw new Promise((resolve) =>
+      setTimeout(() => {
+        done = true
+        resolve('done')
+      }, 20000)
+    )
+
+  // const { data, isError } = useSuspenseQuery({
+  //   queryKey: ['2'],
+  //   queryFn: async () => {
+  //     await new Promise((resolve) => setTimeout(resolve, 3000))
+  //     return { name: 'John Doe' }
+  //   },
+  // })
 
   return <div>done</div>
 }
